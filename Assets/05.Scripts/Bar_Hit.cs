ee5ca4b46fb72863b95441e5519504cc2ef7b7b3
lazyArtisan <biomatrix117@naver.com>
2024-12-19 16:10:53 +0900

    판정 구현

--- Assets/05.Scripts/Bar_Hit.cs
+++ Assets/05.Scripts/Bar_Hit.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class Bar_Hit : MonoBehaviour
 {
-    [SerializeField] Collider2D bar_collider;
     [SerializeField] float rayLength;
     // private LayerMask targetLayer;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void Update()
@@ -27,6 +20,7 @@ public class Bar_Hit : MonoBehaviour
     void CheckHit()
     {
         Vector3 rayOrigin = transform.position;
+        Debug.Log("Ray Origin : " + rayOrigin);
         Vector3 rayDirection = Vector3.back;
         Ray ray = new Ray(rayOrigin, rayDirection);
         RaycastHit hit;
@@ -34,10 +28,44 @@ public class Bar_Hit : MonoBehaviour
         if (Physics.Raycast(ray, out hit, rayLength))
         {
             Debug.Log("Hit : " + hit.collider.name);
+            Judge(hit);
         }
         else
         {
             Debug.Log("No Hit");
         }
+    }
+
+    /// <summary>
+    /// 노트 블럭 중심과의 거리를 계산하여 판정
+    /// </summary>
+    void Judge(RaycastHit hit)
+    {
+        float distance = Vector2.Distance(hit.collider.bounds.center, hit.point);
+        Debug.Log("Distance : " + distance);
+        switch (distance)
+        {
+            case float d when d < 0.02f:
+                Debug.Log("Perfect");
+                break;
+            case float d when d < 0.06f:
+                Debug.Log("Nice");
+                break;
+            case float d when d < 0.1f:
+                Debug.Log("Good");
+                break;
+            default:
+                Debug.Log("Bad");
+                break;
+        }
+    }
+
+    GameObject GetRootParent(GameObject obj)
+    {
+        while (obj.transform.parent.name != "MapRoot")
+        {
+            obj = obj.transform.parent.gameObject;
+        }
+        return obj; // 최상위 부모 반환
     }
 }
