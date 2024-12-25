fc90d08955d3075053b38fc73f0e45952eaecb71
lazyArtisan <biomatrix117@naver.com>
2024-12-26 02:28:18 +0900

    카메라 시점 고정 풀기

---
+++ Assets/05.Scripts/Map/MapEditorCamHandler.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCameraHandler : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    [SerializeField] Cinemachine.CinemachineVirtualCamera virtualCamera;
+    [SerializeField] GameObject player;
+    bool isFollowingPlayer = true;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isFollowingPlayer = !isFollowingPlayer;
+        }
+
+        if (isFollowingPlayer && virtualCamera.Follow == gameObject.transform)
+        {
+            player.GetComponent<Bar_Judge_Movement>().enabled = true;
+            virtualCamera.Follow = player.transform;
+        }
+        else if (!isFollowingPlayer && virtualCamera.Follow == player.transform)
+        {
+            player.GetComponent<Bar_Judge_Movement>().enabled = false;
+            gameObject.transform.position = player.transform.position;
+            virtualCamera.Follow = gameObject.transform;
+        }
+
+        if (!isFollowingPlayer)
+        {
+            MoveCamera();
+        }
+    }
+
+    void MoveCamera()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(x, y, 0) * moveSpeed * Time.deltaTime;
+        transform.Translate(move);
+    }
+}
